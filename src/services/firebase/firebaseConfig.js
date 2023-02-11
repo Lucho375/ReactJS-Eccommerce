474229c9245c9e1f116412dfb76ed2c31eee420c
Luciano Rodriguez <111153206+Lucho375@users.noreply.github.com>
2023-02-11 01:32:52 -0300

    Update firebaseConfig.js

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDsLP73cKMr6tVeTzDppNc8mqDSDxpe69c",
-//     authDomain: "ecommerce-19bab.firebaseapp.com",
-//     projectId: "ecommerce-19bab",
-//     storageBucket: "ecommerce-19bab.appspot.com",
-//     messagingSenderId: "935803654540",
-//     appId: "1:935803654540:web:b9bd257214ac1e707f06a9"
-// };
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
