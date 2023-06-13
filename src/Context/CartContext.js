1869291a557f91116097b1e2ee5a32179831e644
Luciano Rodriguez <111153206+Lucho375@users.noreply.github.com>
2023-06-13 18:37:38 -0300

    Update CartContext.js
    update useCart hook

--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -4,11 +4,11 @@ import { useCart } from "../hooks/useCart";
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-    const {addItem, isInCart, totalQuantity, cart, removeItemCart, total, clearCart, increaseQuantity, decreaseQuantity } = useCart()
+    const cart = useCart()
 
     return (
         <CartContext.Provider 
-        value={{ addItem, isInCart, totalQuantity, cart, removeItemCart, total, clearCart, increaseQuantity, decreaseQuantity }}>
+        value={cart}>
             {children}
         </CartContext.Provider>
     )
